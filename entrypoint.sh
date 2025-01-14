8e09c7bef5dfa435f21f4b3e11dd2a0719751c4f
Miguel Angel Rodriguez <58237801+rodriguez-miguel-angel@users.noreply.github.com>
2025-01-14 20:11:11 +0000

    Modified entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,14 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/feed.py
+
+
+# python3 /usr/bin/feed.py
+
+# Activate the virtual environment 
+source /opt/venv/bin/activate
+python3 /app/feed.py
+
 
 git add -A && git commit -m "Update Feed"
 
